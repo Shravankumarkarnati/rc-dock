3fd2c5c1de2ff308680d61b250f77d92d3c68ed1
Shravan Kumar Karnati <shravank@truveta.com>
2024-04-10 23:35:06 -0700

    DockTabPane

--- src/DockTabPane.tsx
+++ src/DockTabPane.tsx
@@ -1,120 +1,103 @@
-import * as React from 'react';
-import classNames from 'classnames';
-import {DockContext, DockContextType, TabPaneCache} from "./DockData";
-import {TabPaneProps} from "rc-tabs";
+import classNames from "classnames";
+import { TabPaneProps } from "rc-tabs";
+import * as React from "react";
+import { TabPaneCache } from "./DockData";
+import { usePortalManager } from "./DockPortalManager";
 
 interface DockTabPaneProps extends TabPaneProps {
-
   cacheId?: string;
   cached: boolean;
-
 }
 
-export default class DockTabPane extends React.PureComponent<DockTabPaneProps, any> {
-  static contextType = DockContextType;
+const DockTabPane = ({
+  cacheId,
+  cached,
+  prefixCls,
+  forceRender,
+  className,
+  style,
+  id,
+  active,
+  animated,
+  destroyInactiveTabPane,
+  tabKey,
+  children,
+}: DockTabPaneProps) => {
+  const { removeTabCache, getTabCache, updateTabCache } = usePortalManager();
 
-  context!: DockContext;
+  const [ref, setRef] = React.useState<null | HTMLElement>(null);
 
-  _ref: HTMLDivElement;
-  getRef = (r: HTMLDivElement) => {
-    this._ref = r;
-  };
+  const cache = React.useRef<null | TabPaneCache>(null);
 
-  updateCache() {
-    const {cached, children, cacheId} = this.props;
-    if (this._cache) {
-      if (!cached || cacheId !== this._cache.id) {
-        this.context.removeTabCache(this._cache.id, this);
-        this._cache = null;
+  React.useEffect(() => {
+    if (cache.current) {
+      if (!cached || cacheId !== cache.current.id) {
+        removeTabCache(cache.current.id, ref);
+        cache.current = null;
       }
     }
-    if (cached && this._ref) {
-      this._cache = this.context.getTabCache(cacheId, this);
-      if (!this._ref.contains(this._cache.div)) {
-        this._ref.appendChild(this._cache.div);
+    if (cached && ref) {
+      cache.current = getTabCache(cacheId, ref);
+
+      if (!ref.contains(cache.current.div)) {
+        ref.appendChild(cache.current.div);
       }
-      this.context.updateTabCache(this._cache.id, children);
+      updateTabCache(cache.current.id, children);
     }
-  }
 
-  visited: boolean;
-
-  _cache: TabPaneCache;
-
-  render() {
-    const {
-      cacheId,
-      cached,
-      prefixCls,
-      forceRender,
-      className,
-      style,
-      id,
-      active,
-      animated,
-      destroyInactiveTabPane,
-      tabKey,
-      children,
-    } = this.props;
-    if (active) {
-      this.visited = true;
-    } else if (destroyInactiveTabPane) {
-      this.visited = false;
-    }
-    const mergedStyle: React.CSSProperties = {};
-    if (!active) {
-      if (animated) {
-        mergedStyle.visibility = 'hidden';
-        mergedStyle.height = 0;
-        mergedStyle.overflowY = 'hidden';
-      } else {
-        mergedStyle.display = 'none';
+    return () => {
+      if (cache.current) {
+        removeTabCache(cache.current.id, ref);
       }
-    }
+    };
+  }, [cached, children, cacheId, removeTabCache, getTabCache, updateTabCache]);
 
-
-    // when cached == undefined, it will still cache the children inside tabs component, but not across whole dock layout
-    // when cached == false, children are destroyed when not active
-    const isRender = cached === false ? active : this.visited;
-
-    let renderChildren: React.ReactNode = null;
-    if (cached) {
-      renderChildren = null;
-    } else if (isRender || forceRender) {
-      renderChildren = children;
-    }
-
-    let getRef = cached ? this.getRef : null;
-    return (
-      <div ref={getRef}
-           id={cacheId}
-           role="tabpanel"
-           aria-labelledby={id && `${id}-tab-${tabKey}`}
-           aria-hidden={!active}
-           style={{...mergedStyle, ...style}}
-           className={classNames(
-             `${prefixCls}-tabpane`,
-             active && `${prefixCls}-tabpane-active`,
-             className,
-           )}
-      >
-        {(active || this.visited || forceRender) && renderChildren}
-      </div>
-    );
+  let visited = false;
+  if (active) {
+    visited = true;
+  } else if (destroyInactiveTabPane) {
+    visited = false;
   }
 
-  componentDidMount(): void {
-    this.updateCache();
-  }
-
-  componentDidUpdate(prevProps: Readonly<DockTabPaneProps>, prevState: Readonly<any>, snapshot?: any): void {
-    this.updateCache();
-  }
-
-  componentWillUnmount(): void {
-    if (this._cache) {
-      this.context.removeTabCache(this._cache.id, this);
+  const mergedStyle: React.CSSProperties = {};
+  if (!active) {
+    if (animated) {
+      mergedStyle.visibility = "hidden";
+      mergedStyle.height = 0;
+      mergedStyle.overflowY = "hidden";
+    } else {
+      mergedStyle.display = "none";
     }
   }
-}
 
+  // when cached == undefined, it will still cache the children inside tabs component, but not across whole dock layout
+  // when cached == false, children are destroyed when not active
+  const isRender = cached === false ? active : visited;
+
+  let renderChildren: React.ReactNode = null;
+  if (cached) {
+    renderChildren = null;
+  } else if (isRender || forceRender) {
+    renderChildren = children;
+  }
+
+  return (
+    <div
+      ref={cached ? setRef : null}
+      id={cacheId}
+      role="tabpanel"
+      aria-labelledby={id && `${id}-tab-${tabKey}`}
+      aria-hidden={!active}
+      style={{ ...mergedStyle, ...style }}
+      className={classNames(
+        `${prefixCls}-tabpane`,
+        active && `${prefixCls}-tabpane-active`,
+        className
+      )}
+    >
+      {(active || visited || forceRender) && renderChildren}
+    </div>
+  );
+};
+
+export default DockTabPane;
