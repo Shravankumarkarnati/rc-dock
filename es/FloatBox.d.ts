73e4e82b0338f3fd396a8bb08b62a222a3107798
Shravan Kumar Karnati <shravank@truveta.com>
2024-10-16 10:17:20 -0700

    floatbox

--- es/FloatBox.d.ts
+++ es/FloatBox.d.ts
@@ -3,7 +3,5 @@ import { BoxData } from "./DockData";
 interface Props {
     boxData: BoxData;
 }
-export declare class FloatBox extends React.PureComponent<Props, any> {
-    render(): React.ReactNode;
-}
+export declare const FloatBox: React.NamedExoticComponent<Props>;
 export {};
